cb46e5c04d2f450acbd6b55b5f166c4faf3db2b5
Arturs Endzelins <arturs.endzelins@gmail.com>
2022-01-07 12:12:58 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="content-container">
+    <div class="content-wrapper">
+      <Homepage />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Homepage from "@/views/Homepage/Homepage.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    Homepage,
   },
 });
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Inter", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.content-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.content-wrapper {
+  padding: 36px 24px;
 }
 </style>
---
+++ src/views/Homepage/Homepage.vue
@@ -0,0 +1,40 @@
+<template>
+  <form class="form">
+    <div class="form__input-wrapper">
+      <div>
+        <label for="name" class="form-label">Name</label>
+        <input
+          type="email"
+          class="form-control"
+          id="name"
+          placeholder="Please enter your name"
+          aria-describedby="nameHelp"
+        />
+        <div id="nameHelp" class="form-text">
+          We'll never share your name with anyone else.
+        </div>
+      </div>
+      <select class="form-select" aria-label="Default select example">
+        <option selected hidden disabled>Please choose test</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+    </div>
+    <div class="form__submit-wrapper">
+      <button type="submit" class="btn btn-primary">Start</button>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Homepage",
+});
+</script>
+
+<style lang="scss">
+@import "./Homepage";
+</style>
